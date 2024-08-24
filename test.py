cb5086e09291477fc2692000aafd7071bc8696a6
radiancegeorge <radiancegeorge@outlook.com>
2024-08-24 22:20:07 +0100

    updates

--- test.py
+++ test.py
@@ -7,7 +7,7 @@ def call_chat_completion (role="user", question=""):
         n_gpu_layers=-1, # Uncomment to use GPU acceleration
         # seed=1337, # Uncomment to set a specific seed
         # n_batch=32,
-        n_ctx=1000, # Uncomment to increase the context window
+        n_ctx=2048, # Uncomment to increase the context window
         split_mode=2
 )
 
